e62b955b9bf81a591c55ee3778124ab086eadb0e
HYF <alinamarasca@gmail.com>
2021-06-16 15:19:53 +0200

    fix more spaces

--- src/procedures/addTodo.js
+++ src/procedures/addTodo.js
@@ -1,12 +1,12 @@
 const addTodo = () => {
-// create div for each todoList
+  // create div for each todoList
 
   const todoList = document.querySelector(".todo-list");
   const newTodo = document.createElement("div");
   newTodo.classList.add("todo");
   todoList.appendChild(newTodo);
 
-// create li inside div of todoList
+  // create li inside div of todoList
 
   const liTodo = document.createElement("li");
   const todoInput = document.querySelector(".todo-input");
