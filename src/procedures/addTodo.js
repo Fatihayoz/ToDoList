cedafb75c0f7dde2ad1535dfc28e33a8d24fe5c0
Mayuri Patil <mayuri.patil2510@gmail.com>
2021-06-17 10:58:28 +0200

    add button

--- src/procedures/addTodo.js
+++ src/procedures/addTodo.js
@@ -1,4 +1,4 @@
-const addTodo = () => {
+const addTodo = (e) => {
   // create div for each todoList
 
   const todoList = document.querySelector(".todo-list");
@@ -9,10 +9,33 @@ const addTodo = () => {
   // create li inside div of todoList
 
   const liTodo = document.createElement("li");
-  const todoInput = document.querySelector(".todo-input");
-  liTodo.innerText = todoInput.value;
-  todoInput.value = "";
-  newTodo.appendChild(liTodo);
+  const button=document.createElement('button');
+  button.innerHTML="X";
+  liTodo.appendChild(button);
+
+  //const todoInput = document.querySelector(".todo-input");
+  //liTodo.innerText = todoInput.value;
+  //todoInput.value = "";
+   //newTodo.appendChild(liTodo);
+  
+   const newInput = document.createElement("input");
+   liTodo.appendChild(newInput);
+
+        const addButton = e.target;
+        const addButtonParent = addButton.parentElement;
+        const todoList = addButtonParent.parentElement;
+        todoList.insertBefore(liTodo,addButtonParent);
+        
+  // const addButton=e.target;
+  // const addButtonParent = addButton.parentElement;
+
+  // const doList = addButtonParent.parentElement;
+  // doList.insertBefore(liTodo,addButtonParent);
+
+  // const todoInput = document.querySelector(".todo-input");
+  // liTodo.innerText = todoInput.value;
+  // todoInput.value = "";
+  // newTodo.appendChild(liTodo);
 };
 
 export { addTodo };
