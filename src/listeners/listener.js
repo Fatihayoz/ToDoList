ad6d5998c113adf8695066bf4cc99674d54ccc8b
Mayuri Patil <mayuri.patil2510@gmail.com>
2021-06-17 11:40:02 +0200

    added listener for delete

--- src/listeners/listener.js
+++ src/listeners/listener.js
@@ -3,3 +3,7 @@ import { addButtonHandler } from "../handlers/add_buttonhandler.js";
 // listener for add button
 const addButton = document.querySelector(".todo-button");
 addButton.addEventListener("click", addButtonHandler);
+
+// event listener for delete button
+//const deleteButton=
+ document.getElementsByClassName("trash-btn").addEventListener('click',deleteTodoHandler);
