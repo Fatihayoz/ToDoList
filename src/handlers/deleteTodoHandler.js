32d6183abaa62823d54955eb25a82c4f6fea636b
Mayuri Patil <mayuri.patil2510@gmail.com>
2021-06-17 14:05:07 +0200

    chneges

--- src/handlers/deleteTodoHandler.js
+++ src/handlers/deleteTodoHandler.js
@@ -1,15 +1,12 @@
+// import { addTodo } from "../procedures/addTodo.js";
+const deleteTodoHandler = (event) => {
+  const target = event.target;
 
-import { addTodo } from "../procedures/addTodo.js";
-const deleteTodoHandler = event => {
+  if (target.nodeName === "BUTTON") {
+    const liToRemove = target.parentElement;
 
-    const target = event.target;
-   
-    if (target.nodeName === 'BUTTON') {
-      const liToRemove = target.parentElement;
-    
     const todoList = document.getElementById("list");
-      todoList.removeChild(liToRemove);
- 
-}
+    todoList.removeChild(liToRemove);
+  }
 };
-    export { deleteTodoHandler };
+export { deleteTodoHandler };
