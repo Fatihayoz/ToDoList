e7b9bfb0ddf6c21f6f135b4a767721b0e558e813
Mayuri Patil <mayuri.patil2510@gmail.com>
2021-06-17 12:14:38 +0200

    added delete handler

--- src/handlers/deleteTodoHandler.js
+++ src/handlers/deleteTodoHandler.js
@@ -3,10 +3,12 @@ import { addTodo } from "../procedures/addTodo.js";
 const deleteTodoHandler = event => {
 
     const target = event.target;
-    if (target.nodeName === 'BUTTON' && target.innerHTML === '<i class='fas fa-trash'></i>') {
+    if (target.nodeName === 'BUTTON' && target.innerHTML === "<i class='fas fa-trash'></i>") {
       const liToRemove = target.parentElement;
       //const todoList = document.getElementById("liTodo");
       const todolist =document.querySelector(".trash-btn");
       todolist.removeChild(liToRemove);
-    }
+      addTodo();
+    };
+}
     export { deleteTodoHandler };
