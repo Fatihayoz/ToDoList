b4a101d52070bef484c39fdb81cca7ce80bf3e8e
Mayuri Patil <mayuri.patil2510@gmail.com>
2021-06-17 11:28:09 +0200

    button added

--- src/procedures/addTodo.js
+++ src/procedures/addTodo.js
@@ -12,38 +12,35 @@ const addTodo = (event) => {
   liTodo.innerText = todoInput.value;
   todoInput.value = "";
   newTodo.appendChild(liTodo);
-
-
-
-  event.preventDefault();
-  // todo div
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
-  // create <li></li>
-  const newTodo = document.createElement("li");
-  newTodo.innerText = todoInput.value;
-  newTodo.classList.add("todo-item");
-  todoDiv.appendChild(newTodo);
-  // add todo to local storage
-  saveLocalTodos(todoInput.value);
-  //check mark button
-  const completedButton = document.createElement("button");
-  completedButton.innerHTML = "<i class='fas fa-check'></i>";
-  completedButton.classList.add("complete-btn");
-  todoDiv.appendChild(completedButton);
-  //trash button
+// trash button
   const trashButton = document.createElement("button");
   trashButton.innerHTML = "<i class='fas fa-trash'></i>";
   trashButton.classList.add("trash-btn");
-  todoDiv.appendChild(trashButton);
+  // append to div
+  newTodo.appendChild(newTodo);
+
+
+
+
+  //event.preventDefault();
+  // todo div
+  //const todoDiv = document.createElement("div");
+  //todoDiv.classList.add("todo");
+  // create <li></li>
+  //const newTodo = document.createElement("li");
+  //newTodo.innerText = todoInput.value;
+  //newTodo.classList.add("todo-item");
+ // todoDiv.appendChild(newTodo);
+
+  //trash button
+  //const trashButton = document.createElement("button");
+  //trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+  //trashButton.classList.add("trash-btn");
+  //todoDiv.appendChild(trashButton);
   // append to list
-  todoList.appendChild(todoDiv);
+  //todoList.appendChild(todoDiv);
   // clear todo input value
-  todoInput.value = "";
-
-
-
-
+  //todoInput.value = "";
 
 
 };
