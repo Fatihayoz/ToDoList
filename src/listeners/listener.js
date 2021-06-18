53ee89cfbc127b14fd0caa7f91399b915d2aa449
Fatih <fatihayoz20800@gmail.com>
2021-06-18 13:43:08 +0200

    resetButtonHandler is added

--- src/listeners/listener.js
+++ src/listeners/listener.js
@@ -1,9 +1,10 @@
 import { addButtonHandler } from "../handlers/add_buttonhandler.js";
+import { resetButtonHandler } from "../handlers/resetButtonHandler.js";
+
 // listener for add button
 const addButton = document.querySelector(".todo-button");
 addButton.addEventListener("click", addButtonHandler);
 
 // listener for reset button
-
 const resetButton = document.querySelector(".reset");
 resetButton.addEventListener("click", resetButtonHandler);
