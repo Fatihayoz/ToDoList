840860290ae89fd89085d694f1052170ec7fe48e
alinamarasca <75176258+alinamarasca@users.noreply.github.com>
2021-06-21 20:39:05 +0200

    i pushed wrong changes
    and can't undo using github, so i will do it manually

--- src/handlers/add_buttonhandler.js
+++ src/handlers/add_buttonhandler.js
@@ -1,12 +1,10 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/named */
 import { addTodo } from "../procedures/addTodo.js";
-import { toStorage } from "../procedures/local-storage.js";
 
 const addButtonHandler = (e) => {
   e.preventDefault();
-  // addTodo();
-  toStorage();
+  addTodo();
 };
 
 export { addButtonHandler };
