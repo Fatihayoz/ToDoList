d19d28ce2539d2b144af0999a007212dcfe15dd3
alinamarasca <75176258+alinamarasca@users.noreply.github.com>
2021-06-22 09:02:57 +0200

    Delete data.js

--- src/data.js
+++
@@ -1,7 +0,0 @@
-//import { addButtonHandler } from "./handlers/add_buttonhandler.js";
-const todo = document.querySelector('.todo-list');
-let todoList = [];
-    if(localStorage.getItem('todo')){
-        todoList = JSON.parse(localStorage.getItem('todo'));
-        ////todo.innerHTML = addButtonHandler();
-    }
