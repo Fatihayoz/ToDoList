cc6f6d7191321a7ffcfaabad83ab3f669c608884
HYF <alinamarasca@gmail.com>
2021-06-30 13:07:48 +0200

    render-list

--- src/components/render-list.js
+++ src/components/render-list.js
@@ -1,19 +1,23 @@
-import { makeTrashButton } from "./trash-button.js";
+// import { makeTrashButton } from "./trash-button.js";
+// import { makeCheckButton } from "./check-button.js";
+// import { state } from "../data.js";
 
-const renderList = (parentEl, array = []) => {
-  parentEl.innerHTML = ""; // should become universal
+const renderList = (data) => {
+  // const array = Array.from(data);
   const ul = document.createElement("ul");
-  // document.querySelector(".todo-container").appendChild(ul);
-  for (const item of array) {
-    const itemDiv = document.createElement("div");
-    itemDiv.setAttribute("id", "new-todo");
+  for (const item of data) {
     const li = document.createElement("li");
-    itemDiv.appendChild(li);
-    itemDiv.appendChild(makeTrashButton());
-    li.innerHTML = item;
-    ul.appendChild(itemDiv);
+    li.dataset.index = data.indexOf(item);
+    const div = document.createElement("div");
+    div.innerHTML = item.text;
+    ul.appendChild(li);
+    li.appendChild(div);
+    // li.appendChild(makeTrashButton());
+    // li.appendChild(makeCheckButton());
   }
   return ul;
 };
 
+// console.log(state);
+// console.log(state.items);
 export { renderList };
