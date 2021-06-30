f5e4ffca81ff7e4191ff18e4cd67a7de02948b66
HYF <alinamarasca@gmail.com>
2021-06-30 12:51:53 +0200

    btn and handler, render uncommented

--- src/components/add-button.js
+++ src/components/add-button.js
@@ -4,7 +4,6 @@ const makeAddButton = (classEl) => {
   const addButton = document.createElement("button");
   addButton.className = "todo-button";
   addButton.classList.add(classEl);
-  // addButton.setAttribute(type);
   addButton.innerHTML = "<i class='fas fa-plus'></i>";
   addButton.addEventListener("click", addButtonHandler);
   return addButton;
--- src/handlers/addButtonHandler.js
+++ src/handlers/addButtonHandler.js
@@ -1,15 +1,16 @@
 import { state } from "../data.js";
-import { renderList } from "../components/render-list.js";
+// import { renderList } from "../components/render-list.js";
 
 const addButtonHandler = (e) => {
   e.preventDefault();
   const todoInput = document.querySelector(".todo-input");
-  state.items.push(todoInput.value); // into state
+  const newDataObject = { text: todoInput.value, done : false} 
+  state.items.push(newDataObject);
   console.log(state.items);
-  // add button
   const listParent = document.querySelector(".todo-container");
-  listParent.appendChild(renderList(listParent, state.items));
   todoInput.value = "";
+  // listParent.innerHTML = todoInput.value;
+  // listParent.appendChild(renderList(state.items));
 };
-
 export { addButtonHandler };
+
