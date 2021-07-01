b7ad33ac1098bb41e38c6f60a147504cf2d73bfb
Sheyla <shel.busta@gmail.com>
2021-07-01 13:00:05 +0200

    fixing in master

--- src/handlers/deleteTodoHandler.js
+++ src/handlers/deleteTodoHandler.js
@@ -1,20 +1,21 @@
-// import { renderList } from "../components/render-list.js";
+import { renderList } from "../components/render-list.js";
 
 import { state } from "../data.js";
 
+/* eslint-disable prefer-destructuring */
 const deleteTodoHandler = (event) => {
-  const target = event.target;
-  if (target.nodeName === "BUTTON") {
-    const listParent = document.querySelector(".todo-container");//
-    listParent.textContent = ""; //
-    console.log(target.parentElement.dataset.index);
-    const toDelete = target.parentElement.dataset.index;
-    state.items.splice(toDelete,1);
- 
-    console.log(state);
-    // listParent.appendChild(renderList(state.items));
-    // renderList(state);
+const target = event.target;
+if (target.nodeName === "BUTTON") {
+const listParent = document.querySelector(".todo-container");//
+listParent.textContent = ""; //
+console.log(target.parentElement.dataset.index);
+const toDelete = target.parentElement.dataset.index;
+state.items.splice(toDelete,1);
+
+console.log(state);
+listParent.appendChild(renderList(state.items));
+// renderList(state);
 
 };
 }
-export { deleteTodoHandler };
+export { deleteTodoHandler }; 
