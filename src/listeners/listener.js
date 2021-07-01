a03fa40990e715ebab54b710a54e9d965418b70f
Fatih <fatihayoz20800@gmail.com>
2021-07-01 14:15:21 +0200

    update ui for resetButton

--- src/listeners/listener.js
+++ src/listeners/listener.js
@@ -1,5 +1,5 @@
-import { resetButtonHandler } from "../handlers/resetButtonHandler.js";
+// import { resetButtonHandler } from "../handlers/resetButtonHandler.js";
 
-// listener for reset button
-const resetButton = document.querySelector(".reset");
-resetButton.addEventListener("click", resetButtonHandler); 
+// // listener for reset button
+// const resetButton = document.querySelector(".reset");
+// resetButton.addEventListener("click", resetButtonHandler); 
