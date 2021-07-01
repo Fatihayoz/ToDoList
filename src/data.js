458d59b5b9d162e668d1bdcbb230b7258db3616e
alinamarasca <75176258+alinamarasca@users.noreply.github.com>
2021-07-01 09:36:15 +0200

    fix conflict

--- src/data.js
+++ src/data.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // const state = { items: [{ text: "", done: false }] };
 
 const schema = {
@@ -26,29 +25,3 @@ const schema = {
     ],
     };
   export { state };
-  
-=======
-// const state = { items: [{ text: "", done: false }] };
-
-const schema = {
-  title: "list item",
-  type: "object",
-  properties: {
-    item: {
-      type: "string",
-      description: "something user plans to do",
-    },
-    done: {
-      type: "boolean",
-      description: "shows if action is done or not",
-    },
-  },
-  required: ["item"],
-};
-
-const state = {
-  items: [],
-  done: false,
-};
-export { state };
->>>>>>> 824d5589cd503a5b9e09d9f496451ed55a66298b
