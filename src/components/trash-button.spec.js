17eb16dbfaca618e76d2e610e4cd212985f396e8
Sheyla <shel.busta@gmail.com>
2021-07-01 10:22:01 +0200

    add trash-button-handler-and-component

---
+++ src/components/trash-button.spec.js
@@ -0,0 +1,19 @@
+
+import { makeTrashButton } from "./trash-button.js";
+
+describe("creates a trash-button with trash favicon", () => {
+  describe("creates trash button", () => {
+    const actual = makeTrashButton();
+
+    it("is as button", () => {
+      expect(actual.nodeName).toEqual("BUTTON");
+    });
+    it('has "fas fa-trash" favicon', () => {
+      // eslint-disable-next-line prettier/prettier
+      expect(actual.innerHTML).toEqual("<i class=\"fas fa-trash\"></i>");
+    });
+    it('has class "trash-btn"', () => {
+      expect(actual.className).toEqual("trash-btn");
+    });
+  });
+});
