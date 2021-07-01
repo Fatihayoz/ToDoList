00f1709221fce07add4a811ae778dc9dfbd3bd43
Fatih <fatihayoz20800@gmail.com>
2021-07-01 14:30:04 +0200

    update for reset button

--- src/handlers/resetButtonHandler.js
+++ src/handlers/resetButtonHandler.js
@@ -4,6 +4,8 @@ const resetButtonHandler = () => {
   while (state.items.length !== 0) {
     state.items.pop();
   }
+  document.querySelector(".todo-container").innerHTML = "";
+  document.querySelector(".todo-input").value = "";
 };
 
 export { resetButtonHandler };
