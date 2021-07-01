3d8d280956c2c08a5b76d2f89a412b82fc62cc98
Sheyla <shel.busta@gmail.com>
2021-07-01 10:08:25 +0200

    try to pull add button again

--- src/handlers/addButtonHandler.js
+++ src/handlers/addButtonHandler.js
@@ -1,16 +1,17 @@
 import { state } from "../data.js";
-// import { renderList } from "../components/render-list.js";
-
+import { renderList } from "../components/render-list.js";
 const addButtonHandler = (e) => {
   e.preventDefault();
   const todoInput = document.querySelector(".todo-input");
+ 
   const newDataObject = { text: todoInput.value, done : false} 
   state.items.push(newDataObject);
   console.log(state.items);
   const listParent = document.querySelector(".todo-container");
   todoInput.value = "";
-  // listParent.innerHTML = todoInput.value;
-  // listParent.appendChild(renderList(state.items));
+  listParent.innerHTML = todoInput.value;
+  listParent.appendChild(renderList(state.items));
+ 
 };
 export { addButtonHandler };
 
