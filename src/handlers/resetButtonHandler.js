ba007f90ebcc2351cfcd0d54ad1ecbf4a2d4b511
Fatih <fatihayoz20800@gmail.com>
2021-07-02 13:40:41 +0200

    update for renderList

---
+++ src/handlers/resetButtonHandler.js
@@ -0,0 +1,24 @@
+import { state } from "../data.js";
+import { renderList } from "../components/render-list.js";
+
+const resetButtonHandler = (e) => {
+  e.preventDefault();
+  while (state.items.length !== 0) {
+    state.items.pop();
+  }
+
+  const resetContainer = document.querySelector(".todo-container");
+  const resetInput = document.querySelector(".todo-input");
+  console.log("state elements after reset: ", state.items);
+  resetContainer.appendChild(renderList(state.items));
+  resetInput.appendChild(renderList(state.items));
+  console.log("ul after reset", renderList(state.items));
+  console.log("resets container: ", resetContainer);
+  console.log("resets input field: ", resetInput);
+
+  // document.querySelector(".todo-container").innerHTML = "";
+  // document.querySelector(".todo-input").value = "";
+  console.log("resetButton works!");
+};
+
+export { resetButtonHandler };
