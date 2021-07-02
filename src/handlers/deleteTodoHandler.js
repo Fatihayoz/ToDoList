29e4efa34e0392eff6b6d88cc7ff86ce9cc6b452
Sheyla <shel.busta@gmail.com>
2021-07-02 11:57:33 +0200

    checked item render from data

--- src/handlers/deleteTodoHandler.js
+++ src/handlers/deleteTodoHandler.js
@@ -4,10 +4,12 @@ import { state } from "../data.js";
 
 /* eslint-disable prefer-destructuring */
 const deleteTodoHandler = (event) => {
+    debugger;
 const target = event.target;
 if (target.nodeName === "BUTTON") {
+
 const listParent = document.querySelector(".todo-container");//
-listParent.textContent = ""; //
+
 console.log(target.parentElement.dataset.index);
 const toDelete = target.parentElement.dataset.index;
 state.items.splice(toDelete,1);
